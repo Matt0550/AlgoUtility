42198938d81cc367808b8b102092c5cad1a815a2
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2022-04-06 20:46:49 +0200

    Add files via upload

---
+++ AlgoUtility.cpp
@@ -0,0 +1,38 @@
+/*
+AlgoUtility Demo
+Developed by Matt05 Dev
+
+Website: https://matt05.ml
+Github: @Matt0550
+*/
+
+#include <iostream>
+#include "utility/console_colors.cpp"
+#include "utility/password_generator.cpp"
+#include "utility/easy_separator.cpp"
+#include "utility/math_utility.cpp"
+
+
+using namespace std;
+
+int main()
+{
+    // Colors Extension
+    cout << color.RED << "Hello World!\n" << color.ENDC;
+    cout << color.print(color.BRIGHT_YELLOW, "DANGER", color.RED_BG) << endl;
+    cout << color.print(color.BRIGHT_GREEN, "Ciao") << endl<<endl;
+
+    // Easy Separator
+    cout << separator.CustomSeparator("-", 20) << endl<<endl;
+
+    // Password Generator
+    cout << GeneratePassword(10) << endl<<endl;
+
+    // Math Utility
+    cout << math.assoluto(-2)<<endl; // Int
+    cout << math.assoluto(-25.6) << endl; // Double
+    cout << math.cubo(-3.5) << endl; // Double
+    cout << math.hasDivisor(10, 2) << endl; // If N has 2 divisor, return true
+
+
+}
---
+++ utility/math_utility.cpp
@@ -0,0 +1,37 @@
+/*
+AlgoUtility Extension
+Developed by Matt05 Dev
+
+Website: https://matt05.ml
+Github: @Matt0550
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+class MathUtility {
+public:
+	template <class N>
+	auto assoluto(N n) -> decltype((n <= 0) ? -1 * n : n) {
+		return (n <= 0) ? -1 * n : n;
+	}
+
+	template <class N>
+	auto cubo(N n) -> decltype(n) {
+		return n*n*n;
+	}
+
+	int hasDivisor(int n, int d) {
+		int divisori = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				divisori++;
+			}
+		}
+		return (divisori > d) ? true : false;
+	}
+};
+
+MathUtility math; // initializate class
